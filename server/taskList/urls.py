a97263da61a00bdd30e494df1bbb0fd2b12e37e3
Carlos Raul Benitez Krug <115422555+CarlosRBK@users.noreply.github.com>
2023-03-24 12:45:22 -0300

    Delete server directory

--- server/taskList/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import taskListView, registerUserView
-
-app_name = 'taskList'
-
-urlpatterns = [
-    path('taskList/',taskListView, name='taskList'),
-    path('registerUser/', registerUserView, name='registerUser'),
-]
